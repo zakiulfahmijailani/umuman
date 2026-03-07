ce52f0defff27b843d827ad6da8069fc5f304915
Zakiul Fahmi Jailani, MSc <zakiul.j@students.amikom.ac.id>
2026-03-07 16:39:16 +0700

    feat: enable dev navbar in production

--- src/components/dev/DevNavLoader.tsx
+++ src/components/dev/DevNavLoader.tsx
@@ -7,6 +7,5 @@ const DevNavBar = dynamic(() => import('@/components/dev/DevNavBar'), {
 });
 
 export default function DevNavLoader() {
-    if (process.env.NODE_ENV !== 'development') return null;
     return <DevNavBar />;
 }
